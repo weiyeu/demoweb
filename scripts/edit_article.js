a2aede516d5e6c4beb301a4dedea4d6b843e77d9
weiyeu <babamama0527@gmail.com>
2015-12-26 03:05:11 +0800

    update drop area

--- scripts/edit_article.js
+++ scripts/edit_article.js
@@ -1,6 +1,25 @@
 $(function(){
 	$('input#uploadImg').change(function(){
-		alert('changed');
+		var fileHandle = this.files[0];
+		var reader = new FileReader();
+		reader.onload = function(e){
+			// create img element
+			var img = $('<img/>').css({
+				'width' : '95%',
+				'display' : 'inline-block',
+			});
+			// set img src attr
+			img.attr('src',e.target.result);
+			// hide all children in drop-area
+			$('.drop-area')
+			.children()
+			.css({
+				'display' : 'none',
+			});
+			// append img to drop-area
+			$('.drop-area').append(img);
+		};
+		reader.readAsDataURL(fileHandle);
 	});
 	$('.drop-area').click(function(){
 		$('input#uploadImg')[0].click();

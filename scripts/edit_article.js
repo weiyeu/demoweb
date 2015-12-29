a8b10180587a6e2d76e76a04ad38c8887848c6b3
weiyeu <babamama0527@gmail.com>
2015-12-29 22:00:31 +0800

    re-update to fix error update sha<9397a95>

--- scripts/edit_article.js
+++ scripts/edit_article.js
@@ -1,154 +1,187 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Eidt article</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- bootstrap css -->
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-	<!-- font-awesom -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-	<!-- common css -->
-	<link rel="stylesheet" type="text/css" href="css/common.css">
-	<!-- custom css -->
-	<link rel="stylesheet" type="text/css" href="css/edit_article.css">
+/**
+ * show image file on .drop-area element
+ * 
+ * @param fileHandle
+ *            An image file object that is gonna show on drop-area
+ * @param imgSrcObj
+ * 			  image src object that contain local image src link(for pointer purpose)
+ */
+ function showImgOnDropArea(fileHandle, imgSrcObj){
+ 	var reader = new FileReader();
+ 	reader.onload = function(e){
+		// get img src
+		localImgSrc = e.target.result;
+		imgSrcObj.localImgSrc = localImgSrc;
+		// create img element
+		var img = $('<img/>').css({
+			'width' : '95%',
+			'margin' : '10px 0px',
+		});
+		// set img src attr
+		img.attr('src',localImgSrc);
+		// create div container elemnet
+		var imgContainer = $('<div></div>').css({
+			'width' : '100%',
+			'display' : 'table-cell',
+			'vertical-align' : 'middle',
+		});
+		// append img to container
+		imgContainer.append(img);
+		// hide all children in drop-area
+		$('.drop-area')
+		.children()
+		.css({
+			'display' : 'none',
+		});
+		// append img to drop-area
+		$('.drop-area').append(imgContainer);
+		// disable img link input 
+		$('#uploadImgLink').attr('disabled','true');
+	};
+	reader.readAsDataURL(fileHandle);
+}
 
-	<!-- jQuery library -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	<!-- bootstrap javascript -->
-	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-	<!-- common javascript -->
-	<script type="text/javascript" src="scripts/common.js"></script>
-	<!-- custom javascript -->
-	<script type="text/javascript" src="scripts/edit_article.js"></script>
-</head>
-<body>
-	<!-- navigation bar -->
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#homeNavbar">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>                        
-				</button>
-				<a class="navbar-brand" href="#"><i class="fa fa-heart"></i> 一起來晚餐吧!</a>
-			</div>
-			<div class="collapse navbar-collapse" id="homeNavbar">
-				<ul class="nav navbar-nav navbar-right">
-					<li class="active"><a href="home.html"><span class="glyphicon glyphicon-home"></span> 首頁</a></li>
-					<li><a href="#"><span class="glyphicon glyphicon-info-sign"></span> 關於</a></li>
-					<li><a href="profile.html"><span class="glyphicon glyphicon-user"></span>申請帳號</a></li>
-					<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> 登入</a></li>
-				</ul>
-			</div>
-		</div>
-	</nav>
-	<!-- main container -->
-	<div class="container">
-		<div class="jumbotron" >
-			<h1 class="text-center text-bold">寫下你想說的吧 <i class="fa fa-heart"></i></h1>
-		</div>
-		<div class="row">
-			<div class="panel panel-default">
-				<div class="panel-body inner">
-					<form class="form-horizontal">
-						<!-- article title -->
-						<div class="form-group">
-							<label class="control-label col-sm-1" for="title">標題:</label>
-							<div class="col-sm-11">
-								<input type="text" class="form-control" id="title" placeholder="">
-							</div>
-						</div>
-						<!-- insert img modal button -->
-						<div class="form-group">
-							<div class="col-sm-offset-1 col-sm-11">
-								<button type="button" class="btn btn-info" data-toggle="modal" data-target="#insertImgModal"><i class="fa fa-picture-o"></i> 插入圖片</button>
-							</div>
-						</div>
-						<!-- profileImg modal -->
-						<div id="insertImgModal" class="modal fade">
-							<div class="modal-dialog modal-lg">
-								<!-- content -->
-								<div class="modal-content">
-									<!-- header -->
-									<div class="modal-header">
-										<button type="button" class="close" data-dismiss="modal">&times;</button>
-										<h4 class="modal-title">Modal Header</h4>
-									</div>
-									<!-- body -->
-									<div class="modal-body">
-										<!-- drop area -->
-										<div class="form-group">
-											<div class="col-sm-offset-2 col-sm-8">
-												<div class="drop-area">
-													<input class="hidden" type='file' name="uploadImg" id="uploadImg"/>
-													<p>
-														將本機圖片拖曳至此<br>
-														或<br>
-														點擊這裡
-													</p>
-												</div>
-											</div>
-										</div>
-										<!-- hint text -->
-										<div class="form-group">
-											<div class="col-sm-offset-2 col-sm-8" style="text-align:center">
-												<p>或是貼上圖片連結↓</p>
-											</div>
-										</div>
-										<!-- link input -->
-										<div class="form-group">
-											<label class="control-label col-sm-2" for="title">圖片連結:</label>
-											<div class="col-sm-8">
-												<input id="uploadImgLink" name="uploadImgLink" type="text" class="form-control" id="title" placeholder="http://example.com/img.jpg">
-											</div>
-										</div>
-									</div>
-									<!-- footer -->
-									<div class="modal-footer">
-										<button type="button" id="cancel" class="btn btn-default" data-dismiss="modal">取消</button>
-										<button type="button" id="uploadImgConfirm" class="btn btn-default pull-left" data-dismiss="modal">確定</button>
-									</div>
-								</div>
-							</div>
-						</div>
-						<!-- article content -->
-						<div class="form-group">
-							<label class="control-label col-sm-1" for="content">內文:</label>
-							<div class="col-sm-11">
-								<div class="form-control editable" id="editableContent" contenteditable="true" style="width:100%;height:500px;overflow:auto" id="content"></div>
-							</div>
-						</div>
-						<!-- article type -->
-						<div class="form-group">
-							<label class="control-label col-sm-1" for="articleType">文章分類:</label>
-							<div class="col-sm-11">
-								<select class="form-control" id="articleType">
-									<option>未分類</option>
-									<option>餐後心情</option>
-									<option>美食分享</option>
-									<option>美妙旋律</option>
-									<option>我想抒發</option>
-									<option>時尚潮流</option>
-								</select>
-							</div>
-						</div>
-						<!-- subimt button -->
-						<div class="form-group">
-							<div class="col-sm-11 col-sm-offset-1">
-								<input type="submit" class="btn btn-default" value="發表">
-								<input type="submit" class="btn btn-default" value="取消">
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- footer -->
-	<footer class="container-fluid text-center">
-		<p>&copy 2015 Dating</p>
-	</footer>
-</body>
-</html>
+/* document ready */
+$(function(){
+	/* turn off firefox default image resize function*/
+	document.execCommand("enableObjectResizing", false, false);
+	
+	var imgSrcObj = {localImgSrc : null};
+	var linkImgsrc;
+	var uniqueIdNum = 0;
+	// input file changed
+	$('input#uploadImg').change(function(){
+		var fileHandle = this.files[0];
+		showImgOnDropArea(fileHandle,imgSrcObj);
+	});
+	// drop-area event handler
+	$('.drop-area')
+	.on('click', function(){
+		$('input#uploadImg')[0].click();
+	})
+	.on('dragover',function(e){
+		// prevent openning image as a link
+		e.preventDefault();
+	})
+	.on('drop',function(e){
+		// prevent openning image as a link
+		e.preventDefault();
+		var fileHandle = e.originalEvent.dataTransfer.files[0];
+		showImgOnDropArea(fileHandle,imgSrcObj);
+	});
+	// click image in contenteditable div
+	$('#editableContent')
+	.on('click',function(e){
+		// get clicked target
+		var target = e.target;
+		// wrap as jquery object
+		jTarget = $(target);
+		// make sure click on img.inserted
+		if(target.tagName == 'IMG' && target.className == 'inserted'){
+			// select img
+			/*
+			var range = document.createRange();
+			range.selectNode(jTarget[0]);
+			window.getSelection().removeAllRanges();
+			window.getSelection().addRange(range);
+			*/
+			// get resizeable state flag
+			var resizeable = jTarget.data('resizeable');
+			// bind events handler to img.inserted if it is not in resizeable state
+			if(!resizeable){
+				var dragging = false;
+				var startX = 0;
+				var startY = 0;
+				var startWidth = 0;
+				jTarget
+				.on('mousedown touchstart',function(e){
+					e.preventDefault();
+					dragging = true;
+					startX = e.pageX;
+					startY = e.pageY;
+					startWidth = $(this).width();
+				})
+				.css({
+					'border' : '3px dashed lightgray',
+					'cursor' : 'nw-resize',
+				});
+				$(this)
+				.on('mousemove touchmove',function(e){
+					if(dragging){
+						var moveX = e.pageX - startX;
+						var moveY = e.pageY - startY;
+						// resize insertedImg
+						jTarget.css({
+							'width' : (startWidth + moveX)+'px',
+						});
+						// change cursor
+						$(this).css({
+							'cursor' : 'nw-resize',
+						})
+					}
+				})
+				.on('mouseup touchend',function(e){
+					dragging = false;
+					// clear cursor
+					$(this).css({
+						'cursor' : '',
+					});
+				});	
+			}
+			// unbind event handlers and reset img.inserted if it is in resizable state
+			else{
+				jTarget
+				.unbind('mousedown mouseup mousemove')
+				.css({
+					'border' : '',
+					'cursor' : 'pointer',
+				});
+				$(this)
+				.unbind('mousemove mouseup');
+			}
+			// toggle resizeable
+			jTarget.data('resizeable',!resizeable);
+		}
+		// reset and unbind event handlers if img.inserted isn't clicked
+		else{
+			$('img.inserted').each(function(){
+				$(this)
+				.unbind('mousedown mouseup mousemove')
+				.data('resizeable',false)
+				.css({
+					'border' : '',
+					'cursor' : '',
+				});
+			});
+			$(this).unbind('mousemove mouseup');
+		}
+	}); // end of $('editableContent').on('click',function{})
+
+	/* modal */
+	// click insert img confirm
+	$('#uploadImgConfirm').click(function(){
+		// countup id
+		uniqueIdNum++;
+		// create insertedImg
+		var insertedImg = $('<img/>').css({
+			'width' : '100px',
+		});
+		insertedImg.attr({
+			'src' : imgSrcObj.localImgSrc,
+			'class' : 'inserted',
+			'id' : 'id'+uniqueIdNum,
+		});
+		// get editableContent
+		var editableContent = $('#editableContent');
+		// insert into editableContent
+		if(imgSrcObj.localImgSrc){
+			insertNodeOverSelection(insertedImg[0], editableContent[0]);
+		}
+		else if(linkImgsrc){
+
+		}
+		else{
+			alert('妳/你還沒有選取任何圖片唷');
+		}
+	});
+})
